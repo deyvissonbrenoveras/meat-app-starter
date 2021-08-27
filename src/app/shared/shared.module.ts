680ebe4b9fde00ab94cc56bc4d1bfc6334a0d8fb
Deyvisson Breno Veras Meireles <breno@WBRT009965.bayer.cnb>
2021-08-26 22:57:47 -0300

    Fim da seção 8 (Módulos)

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { InputComponent } from './input/input.component';
+import { RadioComponent } from './radio/radio.component';
+import { RatingComponent } from './rating/rating.component';
+import { OrderService } from 'app/order/order.service';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+
+@NgModule({
+    declarations: [InputComponent, RadioComponent, RatingComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    exports: [InputComponent, RadioComponent, RatingComponent,
+             CommonModule, FormsModule, ReactiveFormsModule]
+})
+export class SharedModule{
+    static forRoot(): ModuleWithProviders{
+        return {
+           ngModule: SharedModule,
+           providers: [OrderService, RestaurantsService, ShoppingCartService] 
+        }
+    }
+}
